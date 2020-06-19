d7eb4761fd8b191481c7bc7507a34e624f4bbc76
mellinialfy <48879957+mellinialfy@users.noreply.github.com>
2020-06-20 00:50:11 +0800

    Update data.blade.php

--- resources/views/data.blade.php
+++ resources/views/data.blade.php
@@ -212,7 +212,7 @@
                 
                 <thead>
                     <tr class="table-heads">
-                        <th class="head-item mbr-fonts-style display-6 sorting" rowspan="2" colspan="1">KABUPATEN</th>
+                        <th class="head-item mbr-fonts-style display-6 sorting" rowspan="2" colspan="1">{{strtoupper($levelsebaran)}}</th>
                         <th class="head-item mbr-fonts-style display-6 sorting" rowspan="2" colspan="1">LEVEL</th>
                         <th class="head-item mbr-fonts-style display-6 sorting" rowspan="1" colspan="5">PENYEBARAN</th>
                         <th class="head-item mbr-fonts-style display-6 sorting" rowspan="1" colspan="4">KONDISI</th>
